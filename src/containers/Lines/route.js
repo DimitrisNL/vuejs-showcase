3f1159c2f95b563f37e0bbf0d6ed75ff8f3ee75a
Dimitrios Lytras <dnlytras@gmail.com>
2018-02-20 00:05:14 +0200

    strip comments from lines router

--- src/containers/Lines/route.js
+++ src/containers/Lines/route.js
@@ -7,11 +7,3 @@ export default {
   icon: 'book',
   component: Lines,
 };
-
-/*
-
-  I like to keep the route seperate for each Component-View.
-  Mostly because things can get out of hands for large apps with many routes.
-
-  I combine them all in the container/routes, before importing them in my Router object
-*/
